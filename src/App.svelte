f1edf671752f08a2a5ebc6749ebd4d25609fc9c5
Zachary Fleckenstein <ZachFleck42@Gmail.com>
2023-01-26 18:07:44 -0500

    Adds Firebase hosting to project

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,18 @@
 <script>
   import "./app.css";
+  import { initializeApp } from "firebase/app";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyD3uTEt6BUD1j_TNWG6JDU6avSg6QULbGg",
+    authDomain: "zfleck-iris-app.firebaseapp.com",
+    projectId: "zfleck-iris-app",
+    storageBucket: "zfleck-iris-app.appspot.com",
+    messagingSenderId: "817800640640",
+    appId: "1:817800640640:web:22e1a390f6dd266f188e65",
+    measurementId: "G-CKQSRS45DM",
+  };
+
+  const app = initializeApp(firebaseConfig);
 </script>
 
 <div class="bg-red-800">
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,23 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-  },
-
   preprocess: preprocess({
     defaults: {
       style: 'postcss',
     },
     postcss: true,
   }),
+
+	kit: {
+		adapter: adapter({
+			pages: 'public',
+			assets: 'public',
+			fallback: 'app.html',
+			precompress: true,
+		}),
+	},
 };
 
 export default config;
