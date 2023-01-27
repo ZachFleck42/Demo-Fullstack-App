062dfddd44e409711c629b6781d3ec6de239c9f0
Zachary Fleckenstein <ZachFleck42@Gmail.com>
2023-01-27 01:15:26 -0500

    Refactors main page redirect

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="js">
+  import { goto } from "$app/navigation";
+  import authStore from "/src/lib/stores/auth.js";
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    if (!$authStore.isLoggedIn) {
+      await goto("/signin");
+    }
+  });
+</script>
