50a6ea92798b03050137126886bae1292e9c5a68
Zachary Fleckenstein <ZachFleck42@Gmail.com>
2023-01-27 01:23:35 -0500

    Begins to implement user pages

--- src/lib/components/HorizontalUserCard.svelte
+++ src/lib/components/HorizontalUserCard.svelte
@@ -12,18 +12,16 @@
   class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100"
 >
   <img
-    class="w-36 h-36 m-3 rounded-full shadow-lg"
+    class="w-24 h-24 m-3 rounded-full shadow-lg"
     src={avatar}
     alt="{first_name} {last_name}'s Avatar"
   />
   <div class="flex flex-col justify-between p-4 leading-normal">
-    <h5
-      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
       {first_name}
       {last_name}
     </h5>
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+    <p class="mb-3 font-normal text-gray-700">
       {email}
     </p>
   </div>
