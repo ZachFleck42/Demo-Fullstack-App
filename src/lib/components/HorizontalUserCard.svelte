34cd7aac30b493062b372f416646a37a4b3c370d
Zachary Fleckenstein <ZachFleck42@Gmail.com>
2023-01-27 07:23:49 -0500

    Styling

--- src/lib/components/HorizontalUserCard.svelte
+++ src/lib/components/HorizontalUserCard.svelte
@@ -7,22 +7,25 @@
   let avatar = user.avatar ?? "";
 </script>
 
-<a
-  href="/app/user/{first_name}{last_name}"
-  class="flex flex-row items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 active:bg-gray-300 transition duration-200 ease-in-out"
->
-  <img
-    class="w-24 h-24 m-3 ml-5 rounded-full shadow-lg"
-    src={avatar}
-    alt="{first_name} {last_name}'s Avatar"
-  />
-  <div class="flex flex-col justify-between p-4">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-      {first_name}
-      {last_name}
-    </h5>
-    <p class="font-normal text-gray-700">
-      {email}
-    </p>
-  </div>
-</a>
+<div class="w-auto">
+  <a
+    href="/app/user/{first_name}{last_name}"
+    class="flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 active:bg-gray-300 transition duration-200 ease-in-out"
+  >
+    <img
+      class="w-24 h-24 m-3 ml-5 rounded-full shadow-lg"
+      src={avatar}
+      alt="{first_name} {last_name}'s Avatar"
+    />
+    <div class="flex flex-col justify-between p-4">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        {first_name}
+        {last_name}
+      </h5>
+      <p class="font-normal text-gray-700">
+        {email}
+      </p>
+    </div>
+    <div class="flex flex-row pl-6 space-x-6" />
+  </a>
+</div>
--- src/routes/app/user/[userName]/+page.svelte
+++ src/routes/app/user/[userName]/+page.svelte
@@ -1,4 +1,16 @@
 <script lang="js">
-</script>
+  import { onMount } from "svelte";
+  import userStore from "/src/lib/stores/users.js";
+  import { page } from "$app/stores";
 
-<p>User page</p>
+  let first_name;
+  let last_name;
+  let email;
+  let avatar;
+
+  onMount(() => {
+    let full_name = $page.params.userName.split(/(?=[A-Z])/);
+    first_name = full_name[0];
+    last_name = full_name[1];
+  });
+</script>
