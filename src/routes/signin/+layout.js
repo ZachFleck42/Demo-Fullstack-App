06ed1845ba1a309096e5d6658b05f098c4b3981b
Zachary Fleckenstein <ZachFleck42@Gmail.com>
2023-01-27 23:23:13 -0500

    Improves redirect from signin page if logged in

---
+++ src/routes/signin/+layout.js
@@ -0,0 +1,19 @@
+import { get } from "svelte/store";
+import authStore from "$lib/stores/auth.js";
+
+export const ssr = false;
+export const prerender = false;
+
+export async function load( {params} ) {
+    let user = get(authStore)?.user;
+    if (user) {
+        return {
+            loggedIn: true,
+        }
+    }
+    else {
+        return {
+            loggedIn: false,
+        }
+    }
+}
--- src/routes/signin/+layout.svelte
+++ src/routes/signin/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="js">
   import App from "../../App.svelte";
   import Footer from "/src/lib/components/Footer.svelte";
+  import { goto } from "$app/navigation";
+
+  export let data;
+  let userLoggedIn = data.loggedIn;
+
+  $: if (userLoggedIn) redirect();
+  const redirect = async () => {
+    await goto("/app");
+  };
 </script>
 
-<App>
-  <slot />
-  <Footer />
-</App>
+{#if !userLoggedIn}
+  <App>
+    <slot />
+    <Footer />
+  </App>
+{/if}
