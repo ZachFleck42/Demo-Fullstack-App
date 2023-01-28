e9835902f197502676d4170804108453b66f192d
Zachary Fleckenstein <ZachFleck42@Gmail.com>
2023-01-27 20:57:23 -0500

    Rudimentary user page

--- src/routes/app/user/[userID]/+page.svelte
+++ src/routes/app/user/[userID]/+page.svelte
@@ -6,3 +6,15 @@
   let email = data.email;
   let avatar = data.avatar;
 </script>
+
+<div class="flex flex-col">
+  <div class="mx-auto text-center">
+    <img
+      src={avatar}
+      alt="{first_name} {last_name}'s avatar"
+      class="w-24 h-24 rounded-full"
+    />
+    <p>{first_name} {last_name}</p>
+    <p>{email}</p>
+  </div>
+</div>
