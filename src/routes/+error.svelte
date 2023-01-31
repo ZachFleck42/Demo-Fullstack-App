616fc9244e67ed3d4704a7279461eb57ea0de349
Zachary Fleckenstein <ZachFleck42@Gmail.com>
2023-01-31 03:40:21 -0500

    Adds error pages to data-loading routes

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<h1>{$page.status}: {$page.error.message}</h1>
+<p>
+  An unexpected error has occurred. Please recheck internet connection, refresh
+  the application, and try again.
+</p>
+<p>
+  If you continue to experience issues, please <a href="/app/support"
+    >contact support</a
+  >
+</p>
---
+++ src/routes/app/user/[userID]/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<h1>ERROR: {$page.status}: {$page.error.message}</h1>
+<p>
+  Unable to load data from API. Please recheck internet connection, refresh the
+  application, and try again.
+</p>
+<p>
+  If you continue to experience issues, please <a href="/app/support"
+    >contact support</a
+  >
+</p>
